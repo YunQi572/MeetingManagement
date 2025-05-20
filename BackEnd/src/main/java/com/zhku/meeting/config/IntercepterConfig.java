e85a1a1ffc31dc6284a3422412871448075357b0
YunQi572 <YJunGuo20050702@outlook.com>
2025-05-20 20:18:28 +0800

    V1.1 初始化

---
+++ BackEnd/src/main/java/com/zhku/meeting/config/IntercepterConfig.java
@@ -0,0 +1,43 @@
+package com.zhku.meeting.config;
+
+import com.github.pagehelper.PageInterceptor;
+import com.zhku.meeting.interceptor.TokenInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class IntercepterConfig implements WebMvcConfigurer {
+    private TokenInterceptor tokenInterceptor;
+
+    public IntercepterConfig(TokenInterceptor tokenInterceptor) {
+        this.tokenInterceptor = tokenInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        List<String> excludePath = new ArrayList<>();
+        excludePath.add("/login");
+        excludePath.add("/static/**");
+        excludePath.add("/assets/**");
+        excludePath.add("/manager");
+        registry.addInterceptor(tokenInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(excludePath);
+    }
+
+    // 配置跨域
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:4002", "http://127.0.0.1:4002")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
